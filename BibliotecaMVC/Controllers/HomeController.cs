04dcc554f0d92761787a43e4b3bc561320623e53
KvBryan <kp4892906@gmail.com>
2026-07-16 11:22:41 -0600

    first commit

--- BibliotecaMVC/Controllers/HomeController.cs
+++ BibliotecaMVC/Controllers/HomeController.cs
@@ -10,12 +10,31 @@ public class HomeController : Controller
     {
         return View();
     }
-
-    public IActionResult Privacy()
+    public IActionResult Libros()
     {
         return View();
     }
-
+    public IActionResult Autores()
+    {
+        return View();
+    }
+    public IActionResult Categorias()
+    {
+        return View();
+    }
+    public IActionResult Usuarios()
+    {
+        return View();
+    }
+    public IActionResult Prestamos()
+    {
+        return View();
+    }
+    public IActionResult AcercaDe()
+    {
+        return View();
+    }
+    
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
