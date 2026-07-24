351a0439657337b60ba49aa97ecca513f3823b81
KvBryan <kp4892906@gmail.com>
2026-07-23 18:01:04 -0600

    Segundo Commit: Semana 2 - Patrón MVC y Vistas Razor en ASP.NET Core MVC

---
+++ BibliotecaMVC/Controllers/LibrosController.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Mvc;
+using BibliotecaMVC.Models;
+namespace BibliotecaMVC.Controllers;
+
+public class LibrosController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        List<Libro> libros = new List<Libro>()
+        {
+            new Libro
+            {
+                ID = 1,
+                Titulo = "Clean Code",
+                Autor = "Robert Martin",
+                Categoria = "Programación",
+                Precio = 35.5m,
+                Disponible =  true
+            },
+            new Libro
+            {
+                ID = 2,
+                Titulo = "Cien años de soledad",
+                Autor = "Gabriel García Márquez",
+                Categoria = "Literatura",
+                Precio = 18,
+                Disponible =  false
+            }
+
+        };
+        ViewBag.Nombre = "Kevin Pérez";
+        ViewBag.Libros = libros;
+        return View();
+    }
+}
